e4c583a8c5e76e205e6219a976dd4a0385996cf5
Kristofer Lund <kristofer@fmckl.se>
2025-09-15 09:15:06 +0200

    Require canister calls to be authenticated

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,4 +1,16 @@
-#[ic_cdk::query]
+use candid::Principal;
+use ic_cdk::{api::msg_caller, query};
+
+#[query(guard = "ensure_authenticated")]
 fn greet(name: String) -> String {
     format!("Hello, {name}!")
+}
+
+/// Ensures that the caller is authenticated (not anonymous).
+pub fn ensure_authenticated() -> Result<(), String> {
+    if msg_caller() == Principal::anonymous() {
+        Err("Anonymous principal not allowed to make calls.".into())
+    } else {
+        Ok(())
+    }
 }
